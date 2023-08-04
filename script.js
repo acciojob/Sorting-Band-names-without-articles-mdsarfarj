44a158790f598d7cd78ab07021433676ac29ecea
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-08-04 23:49:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-// Given array of band names
 let bandNames = ['The Beatles', 'Metallica', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones'];
 
-// Function to sort and display band names without articles
-function sortAndDisplayBands(bandNames) {
+function removeArticles(name) {
   const articles = ['a', 'an', 'the'];
+  const words = name.split(' ');
 
-  const sortedBands = bandNames.slice().sort((a, b) => {
-    const getNameWithoutArticle = name => {
-      for (const article of articles) {
-        if (name.toLowerCase().startsWith(article + ' ')) {
-          return name.slice(article.length + 1);
-        }
-      }
-      return name;
-    };
+  // Filter out articles and return the remaining words as a string
+  return words.filter(word => !articles.includes(word.toLowerCase())).join(' ');
+}
 
-    const nameA = getNameWithoutArticle(a);
-    const nameB = getNameWithoutArticle(b);
-
-    return nameA.localeCompare(nameB);
-  });
-
+function sortAndDisplayBands(bands) {
+  const sortedBands = bands.slice().sort((a, b) => removeArticles(a) > removeArticles(b) ? 1 : -1);
   const ulElement = document.getElementById('band');
 
+  // Clear existing list content
+  ulElement.innerHTML = '';
+
+  // Create and append list items for each band
   sortedBands.forEach(band => {
     const liElement = document.createElement('li');
     liElement.textContent = band;
@@ -30,5 +23,5 @@ function sortAndDisplayBands(bandNames) {
   });
 }
 
-// Call the function with the band names array
+// Call the function to sort and display the bands
 sortAndDisplayBands(bandNames);
